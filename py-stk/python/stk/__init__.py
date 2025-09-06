5c90d62f651c109caba5ab8797dfc8f882648430
Lukas Turcani <lukasturcani93@gmail.com>
2025-09-06 22:06:33 +0100

    Initial commit

---
+++ py-stk/python/stk/__init__.py
@@ -0,0 +1,6 @@
+from .stk import *
+
+
+__doc__ = stk.__doc__
+if hasattr(stk, "__all__"):
+    __all__ = stk.__all__
---
+++ py-stk/python/tests/test_all.py
@@ -0,0 +1,5 @@
+import stk
+
+
+def test_sum_as_string():
+    assert stk.sum_as_string(1, 1) == "2"
