191af3ad569585df3c58032fcc0c26704bef0516
Lukas Turcani <lukasturcani93@gmail.com>
2025-09-07 09:33:44 +0100

    WIP

--- crates/stk-error/src/lib.rs
+++ crates/stk-error/src/lib.rs
@@ -1,4 +1,47 @@
+use std::borrow::Cow;
+use std::fmt::{self, Display, Formatter};
+use std::ops::Deref;
 use thiserror::Error;
 
+#[derive(Debug, Clone)]
+pub struct ErrorString(Cow<'static, str>);
+
+impl ErrorString {
+    pub const fn new_static(s: &'static str) -> Self {
+        Self(Cow::Borrowed(s))
+    }
+}
+
+impl<T> From<T> for ErrorString
+where
+    T: Into<Cow<'static, str>>,
+{
+    fn from(msg: T) -> Self {
+        ErrorString(msg.into())
+    }
+}
+
+impl AsRef<str> for ErrorString {
+    fn as_ref(&self) -> &str {
+        &self.0
+    }
+}
+
+impl Deref for ErrorString {
+    type Target = str;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl Display for ErrorString {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
 #[derive(Error, Debug, PartialEq, Eq, Clone)]
-pub enum StkError {}
+pub enum StkError {
+    ParseError(ErrorString),
+}
