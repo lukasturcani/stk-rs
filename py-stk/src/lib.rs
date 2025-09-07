264d80f33cc69236f267722f4c873cf94cc59fdb
Lukas Turcani <lukasturcani93@gmail.com>
2025-09-07 08:05:25 +0100

    WIP

--- py-stk/src/lib.rs
+++ py-stk/src/lib.rs
@@ -1,13 +1,14 @@
 use pyo3::prelude::*;
+use pyo3::wrap_pymodule;
 
 /// A Python module implemented in Rust.
 #[pymodule]
 fn stk(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_wrapped(wrap_pymodule!(molecular_graph));
+    m.add_wrapped(wrap_pymodule!(molecular_graph))?;
     Ok(())
 }
 
 #[pymodule]
-fn molecular_graph(m: &PyModule) -> PyResult<()> {
+fn molecular_graph(_: &Bound<'_, PyModule>) -> PyResult<()> {
     Ok(())
 }
