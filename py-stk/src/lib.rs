78ea9c09b7de6cfb3657183b76a34bae69effb50
Lukas Turcani <lukasturcani93@gmail.com>
2025-09-07 12:53:21 +0100

    WIP

--- py-stk/src/lib.rs
+++ py-stk/src/lib.rs
@@ -7,6 +7,7 @@ use stk_python::molecular_graph::PyMolecularGraph;
 #[pymodule]
 fn stk(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_wrapped(wrap_pymodule!(molecular_graph))?;
+    m.add_wrapped(wrap_pymodule!(error_module))?;
     Ok(())
 }
 
